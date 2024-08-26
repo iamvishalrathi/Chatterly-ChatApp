14f920013c531c2a96e886e2f598f00468339839
Vishal Rathi <rajatrathi029@gmail.com>
2024-08-26 19:18:40 +0530

    Connected to db

--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,19 @@ import cookieParser from "cookie-parser"
 const app = express();
 dotenv.config();
 
+//Connect Mongo
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => {
+        console.log("Connected to MongoDB")
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+
 app.get("/",(req,res)=>{
     res.send("At root");
 });
-
 
 //Listening Port
 const PORT = process.env.PORT || 3000;
