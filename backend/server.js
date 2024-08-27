37c5bef833f688aada8a155ca3c11dbc63778dc5
Vishal Rathi <rajatrathi029@gmail.com>
2024-08-27 15:55:48 +0530

    send message

--- backend/server.js
+++ backend/server.js
@@ -21,11 +21,11 @@ app.use(cookieParser());
 
 // import routes
 import authRoutes from "./routes/auth.routes.js"
-// import messageRoute from "./routes/message.routes.js"
+import messageRoute from "./routes/message.routes.js"
 // import userRoute from "./routes/user.routes.js"
 
 app.use("/api/auth", authRoutes)
-// app.use("/api/messages", messageRoute)
+app.use("/api/messages", messageRoute)
 // app.use("/api/users", userRoute)
 
 app.get("/",(req,res)=>{
