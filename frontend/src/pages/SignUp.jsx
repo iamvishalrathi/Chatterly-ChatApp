5f63634c20b503df00913b81db51f0138b90e8ac
Vishal Rathi <rajatrathi029@gmail.com>
2024-08-27 21:50:28 +0530

    signup ui

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,11 +1,70 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom"
+import GenderCheckbox from "../component/GenderCheckbox";
 
 const Signup = () => {
-    return (
-        <div>
-            Signup
-        </div>
-    )
-}
+  return (
+    <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+      <div className=" w-full p-6 rounded-lg shadow-md bg-gray-200">
+        <h1 className="text-3xl font-semibold text-center text-slate-700">
+          Signup to
+          <span className="text-blue-700"> Chat Application</span>
+        </h1>
+        <form>
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Username</span>
+            </label>
 
-export default Signup
+            <input
+              type="text"
+              placeholder="Enter Username"
+              className="w-full input input-bordered h-10"
+            />
+          </div>
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Email</span>
+            </label>
+
+            <input
+              type="email"
+              placeholder="example@gmail.com"
+              className="w-full input input-bordered h-10"
+            />
+          </div>
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Password</span>
+            </label>
+
+            <input
+              type="password"
+              placeholder="Enter Password"
+              className="w-full input input-bordered h-10"
+            />
+          </div>
+
+          <GenderCheckbox/>
+
+          <Link
+            to={"/login"}
+            className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block"
+          >
+            Already have an account?
+          </Link>
+
+          <div>
+            <button
+              className="btn btn-block btn-sm mt-2 bg-blue-700 font-bold text-white"
+            >
+              Signup
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
