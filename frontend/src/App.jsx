7d242e16b800ea6559060c4dfb0ad08b3ed5a573
Vishal Rathi <rajatrathi029@gmail.com>
2024-08-27 20:10:47 +0530

    frontend setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,14 @@
-import "./App.css";
-function App() {
-  return(
-    <>
-      <h1>BoilerPlate</h1>
-    </>
+const App = () => {
+  return (
+    <div>
+      <button className="btn btn-active">Default</button>
+      <button className="btn btn-active btn-neutral">Neutral</button>
+      <button className="btn btn-active btn-primary">Primary</button>
+      <button className="btn btn-active btn-secondary">Secondary</button>
+      <button className="btn btn-active btn-accent">Accent</button>
+      <button className="btn btn-active btn-ghost">Ghost</button>
+      <button className="btn btn-active btn-link">Link</button>
+    </div>
   )
 }
 
