e867eae16a28ab739bb0f4d9af779943e5361bea
Vishal Rathi <rajatrathi029@gmail.com>
2024-08-29 22:50:21 +0530

    get msg func

--- frontend/src/component/messages/Messages.jsx
+++ frontend/src/component/messages/Messages.jsx
@@ -1,16 +1,41 @@
-import React from 'react'
-import Message from './Message'
+import { useEffect, useRef } from "react"
+import Message from "./Message"
+import useGetMessages from "../../hooks/useGetMessages"
+// import useListenMessages from "../../hooks/useListenMessages"
 
 const Messages = () => {
+  const { messages, loading } = useGetMessages()
+
+  // console.log(messages)
+
+  // useListenMessages()
+
+  const lastMessageRef = useRef() //This is a React Hook that returns a 
+  // mutable ref object. This ref object has a current property that can hold a 
+  // reference to a DOM element or a value that persists across re-renders.
+
+
+  useEffect(() => {
+    setTimeout(() => {
+      lastMessageRef.current?.scrollIntoView({ behaviour: "smooth" }) //This method scrolls the referenced element into view.
+    })
+  }, [messages])
+
   return (
     <div className="px-4 flex-1 overflow-auto">
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
+      {!loading && messages.length === 0 && (
+        <p className="text-center">Start conversation by sending a message</p>
+      )}
+
+      {!loading &&
+        messages.length > 0 &&
+        messages.map((message) => (
+          <div key={message._id} ref={lastMessageRef}>
+            <Message message={message} />
+          </div>
+        ))}
+
+      {loading && <div className="w-full flex justify-center"><div className="loading loading-spinner"></div></div>}
     </div>
   )
 }
---
+++ frontend/src/hooks/useGetMessages.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react"
+import useConversation from "../zustand/useConversation"
+import toast from "react-hot-toast"
+
+const useGetMessages = () => {
+  const [loading, setLoading] = useState(false)
+  const { messages, setMessages, selectedConversation } = useConversation()
+
+  useEffect(() => {
+    const getMessages = async () => {
+      try {
+        setLoading(true)
+
+        const res = await fetch(`/api/messages/${selectedConversation._id}`)
+
+        const data = await res.json()
+
+        if (data.error) {
+          throw new Error(data.error)
+        }
+        else {
+          setMessages(data) //We didn't de-constructed "messages" as
+          //we don't wanna call useEffect() on storing fetched data
+        }
+      } catch (error) {
+        toast.error(error.message)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    if (selectedConversation?._id) { //To prevent useEffect() being called first
+      // time on loading the page
+      getMessages()
+    }
+  }, [selectedConversation._id, setMessages])
+
+  return { messages, loading }
+}
+
+export default useGetMessages
