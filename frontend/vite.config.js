2c7d2ec4fcbeb1f370919203356c3fc261b74fa8
Vishal Rathi <rajatrathi029@gmail.com>
2024-08-31 20:59:52 +0530

    online server connect

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://chatterly-hp0h.onrender.com",
         secure: false,
       },
     },
